191960c64be9aaa9072135fe97d6aaf545e498e2
Roman lesyo <ladiginscormag@gmail.com>
2023-09-27 15:05:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ const app = express()
 // Set up the express application
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
+app.use(cors(
+	{
+		origin: 'http://localhost:5000, https://our-chat-my.netlify.app',
+		credentials: true,
+	}
+))
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
 
