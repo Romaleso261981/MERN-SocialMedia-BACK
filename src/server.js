e2bbada5f7bd627f1f9d7937e396ada0660f3929
Roman lesyo <ladiginscormag@gmail.com>
2023-10-16 08:48:27 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,64 +1,73 @@
-import { errorsMidleware } from './middlewares/errorsMiddleware.js';
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { Server } from 'socket.io';
-import http from 'http';
-import { dbConnect } from './services/dbConnect.js';
+import { errorsMidleware } from './middlewares/errorsMiddleware.js'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { Server } from 'socket.io'
+import http from 'http'
+import { dbConnect } from './services/dbConnect.js'
 
 // routes
 import {
-  globalRouter,
-  authRouter,
-  roomsRouter,
-  userRouter,
-  privateChatsRouter,
-  roomsChatRouter,
-} from './routes/index.js';
+	globalRouter,
+	authRouter,
+	roomsRouter,
+	userRouter,
+	privateChatsRouter,
+	roomsChatRouter,
+} from './routes/index.js'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
 // Load environment variables
-const PORT = process.env.SERVER_PORT || 8880;
-const startupDevMode = app.get('env') === 'development';
+const PORT = process.env.SERVER_PORT || 8880
+const startupDevMode = app.get('env') === 'development'
 
-dbConnect();
+dbConnect()
 
 // Set up the express application
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(
-  cors({
-    origin: ['https://our-chat-app-two.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
+	cors({
+		origin: [
+			'https://our-chat-app-two.vercel.app',
+			'https://our-chat-my.netlify.app',
+			'http://localhost:3000',
+			'http://localhost:3001',
+		],
+		credentials: true,
+		optionsSuccessStatus: 200,
+	})
+)
 
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
 
 // routes
-app.use('/', globalRouter);
-app.use('/auth', authRouter);
-app.use('/rooms', roomsRouter);
-app.use('/user', userRouter);
+app.use('/', globalRouter)
+app.use('/auth', authRouter)
+app.use('/rooms', roomsRouter)
+app.use('/user', userRouter)
 
 // Necessary to resolve server crash when an error occurs
-app.use(errorsMidleware);
+app.use(errorsMidleware)
 
-const httpServer = http.createServer(app);
+const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
-  cors: {
-    origin: ['https://our-chat-app-two.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
-    optionsSuccessStatus: 200,
-  },
-});
+	cors: {
+		origin: [
+			'https://our-chat-app-two.vercel.app',
+			'https://our-chat-my.netlify.app',
+			'http://localhost:3000',
+			'http://localhost:3001',
+		],
+		optionsSuccessStatus: 200,
+	},
+})
 
-privateChatsRouter(io);
-roomsChatRouter(io);
+privateChatsRouter(io)
+roomsChatRouter(io)
 
-
-httpServer.listen(PORT, () => console.log(`Listening at Port ${PORT}`));
+httpServer.listen(PORT, () => console.log(`Listening at Port ${PORT}`))
